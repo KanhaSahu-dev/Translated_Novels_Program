2b90625ba3ca0042b747f41a78f66129533304ef
Kanha <2414287@cognizant.com>
2025-08-31 21:01:33 +0530

    7

--- frontend/novel-refiner-app/src/app/app.routes.ts
+++ frontend/novel-refiner-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { NovelSearchComponent } from './components/novel-search/novel-search';
+import { NovelListComponent } from './components/novel-list/novel-list';
+import { GlossaryManagerComponent } from './components/glossary-manager/glossary-manager';
+import { ChapterViewComponent } from './components/chapter-view/chapter-view';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: 'search', component: NovelSearchComponent },
+  { path: 'list', component: NovelListComponent },
+  { path: 'glossary', component: GlossaryManagerComponent },
+  { path: 'chapters', component: ChapterViewComponent },
+  { path: '**', redirectTo: '/search' }
+];
